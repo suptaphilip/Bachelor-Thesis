a2f7634bcd17bcc7c6b8ccfd031bed1bd4834c99
Supta Richard Philip <supta.philip@gmail.com>
2014-01-21 14:12:31 +0600

    edit js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
 console.log('This would be the main JS file.');
+
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+   if (links[i].hostname != window.location.hostname) {
+       links[i].target = '_blank';
+   } 
+}
